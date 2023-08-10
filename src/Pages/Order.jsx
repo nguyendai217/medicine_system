c89936ef3e84c44729226353ec302b813827bccf
nguyendai217 <nguyendai2171998@gmail.com>
2023-08-10 21:49:26 +0700

    update export excel order

--- src/Pages/Order.jsx
+++ src/Pages/Order.jsx
@@ -1,23 +1,22 @@
 import { React, useEffect, useState } from 'react';
-import { ComboBoxComponent } from '@syncfusion/ej2-react-dropdowns';
 import { TextBoxComponent } from '@syncfusion/ej2-react-inputs';
 import { DialogComponent } from '@syncfusion/ej2-react-popups';
 import { NumericTextBoxComponent } from '@syncfusion/ej2-react-inputs';
 import { Header } from '../Components';
 import { useStateContext } from "../Contexts/ContextProvider";
-import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter, Resize } from '@syncfusion/ej2-react-grids';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Toolbar, Sort, Filter, Resize } from '@syncfusion/ej2-react-grids';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
 import moment from 'moment';
-import * as FileSaver from "file-saver";
+import FileSaver from "file-saver";
 import * as XLSX from "xlsx";
 
 const Order = () => {
   const { currentColor } = useStateContext();
   const positionDialog = { X: "center", Y: 18 };
   const [visibility, setVisibility] = useState(false);
-  const [productsData, setProductsData] = useState([]);
+  // const [productsData, setProductsData] = useState([]);
   const [productsDataOrder, setProductsDataOrder] = useState([]);
 
   // input data order
@@ -27,69 +26,58 @@ const Order = () => {
   const [buyPrice, setBuyPrice] = useState();
   const [note, setNote] = useState();
 
-  const [indexItem, setIndexItem] = useState(0);
-  const [isOldPrice, setIsOldPrice] = useState(false);
+  const [indexItem, setIndexItem] = useState(1);
   const [dataOldPrice, setDataOldPrice] = useState('');
   const [totalInputPrice, setTotalInputPrice] = useState(0);
   const [totalBuyPrice, setTotalBuyPrice] = useState(0);
   const [totalProfit, setTotalProfit] = useState(0);
   let grid;
-
-  useEffect(() => {
-    retriveData();
-  }, [])
-
-  function retriveData() {
-    axios.get('/products')
-      .then((res) => {
-        setProductsData(res.data);
-      })
-      .catch((error) => {
-        toast.error('Lỗi truy vấn thông tin sản phẩm !', {
-          position: toast.POSITION.TOP_RIGHT
-        });
-      });
-  }
+  const [totalItem, setTotalItem] = useState(0);
 
   function handleOrderProduct() {
-    if (!quantity || !productOrder || !inputPrice || !buyPrice) {
+    if (!productOrder || !quantity || !inputPrice || !buyPrice) {
       toast.error('Vui lòng nhập đầy đủ thông tin !', {
         position: toast.POSITION.TOP_RIGHT
       });
       return;
     }
-    let obj = getProductById(productOrder);
-    obj['id'] = obj.id;
-    obj['product_code'] = obj.product_code;
-    obj['product_name'] = obj.product_name;
-    obj['quantity'] = quantity;
-    obj['input_price'] = inputPrice;
-    obj['buy_price'] = buyPrice;
-    obj['note'] = note;
-    obj['idx'] = indexItem;
-    obj['total_input_item'] = quantity * inputPrice;
-    obj['total_output_item'] = quantity * buyPrice;
-    productsDataOrder.push(obj);
-    setProductsDataOrder(productsDataOrder);
-    grid.refresh();
-    let nextIndex = indexItem + 1;
-    setIndexItem(nextIndex);
-    clearOrder();
 
-    let totalInp = fcTotalInputPrice(productsDataOrder);
-    setTotalInputPrice(totalInp);
-    let totalBuyPr = fcTotalBuyPrice(productsDataOrder);
-    setTotalBuyPrice(totalBuyPr);
-    let totalProfitPrice = totalBuyPr - totalInp;
-    setTotalProfit(totalProfitPrice);
-  }
+    // call api get product by product_code
+    axios.get('/products/productCode/' + productOrder)
+      .then((res) => {
+        if (res.data) {
+          let obj = {};
+          obj['id'] = res.data.id;
+          obj['product_code'] = res.data.product_code;
+          obj['product_name'] = res.data.product_name;
+          obj['unit_name'] = res.data.unit_name;
+          obj['quantity'] = quantity;
+          obj['input_price'] = inputPrice;
+          obj['buy_price'] = buyPrice;
+          obj['note'] = note;
+          obj['idx'] = indexItem;
+          obj['total_input_item'] = quantity * inputPrice;
+          obj['total_output_item'] = quantity * buyPrice;
 
-  function getProductById(productId) {
-    for (var i = 0; i < productsData.length; i++) {
-      if (productsData[i].id == productId) {
-        return productsData[i];
-      }
-    }
+          productsDataOrder.push(obj);
+          setProductsDataOrder(productsDataOrder);
+          grid.refresh();
+          let nextIndex = indexItem + 1;
+          setIndexItem(nextIndex);
+          clearOrder();
+          setTotalItem(totalItem + 1);
+        } else {
+          toast.error('Không có thông tin mã sp !', {
+            position: toast.POSITION.TOP_RIGHT
+          });
+        }
+      })
+      .catch((error) => {
+        alert(error);
+        toast.error('Lỗi truy vấn dữ liệu !', {
+          position: toast.POSITION.TOP_RIGHT
+        });
+      });
   }
 
   function clearOrder() {
@@ -100,27 +88,29 @@ const Order = () => {
     setNote('');
   }
 
-  function checkOldPrice(productId) {
-    if (!productId) {
+  function checkOldPrice() {
+    if (!productOrder) {
       toast.error('Vui lòng chọn sản phẩm !', {
         position: toast.POSITION.TOP_RIGHT
       });
       return;
     }
-    // call api check price
 
-    axios.get('/old_price/' + productId)
+    // call api check price
+    axios.get('/old_price/productCode/' + productOrder)
       .then((res) => {
+        let tmp = '';
         let dataPrice = res.data;
-        if (dataPrice.length > 0) {
-          setIsOldPrice(true);
+        if (dataPrice.length <= 0) {
+          tmp = 'Không có thông tin giá cũ !'
+        } else {
+          let listPrice = [];
+          dataPrice.forEach((item) => {
+            listPrice.push(new Intl.NumberFormat('en-DE').format(item.price) + ' (' + moment(item.input_date).format('DD-MM-YYYY') + ')');
+          });
+          tmp = "Giá cũ: " + listPrice.join(' , ');
         }
-        let listPrice = [];
-        dataPrice.forEach((item) => {
-          listPrice.push(new Intl.NumberFormat('en-DE').format(item.price) + ' (' + moment(item.input_date).format('DD-MM-YYYY') + ')');
-        });
-        let tmp = "Giá cũ: ";
-        setDataOldPrice(tmp + listPrice.join(' , '));
+        setDataOldPrice(tmp);
       })
       .catch((error) => {
         toast.error('Lỗi truy vấn dữ liệu !', {
@@ -175,30 +165,31 @@ const Order = () => {
   };
 
   const deleteOrderProduct = (id) => {
-    let newArr = removeObjectWithId(productsDataOrder, id);
-    setProductsDataOrder(newArr);
-    console.log('newArr', newArr);
+    let listData = [];
+    for (var i = 0; i < productsDataOrder.length; i++) {
+      if (productsDataOrder[i].id !== id) {
+        listData.push(productsDataOrder[i]);
+      }
+    }
+    setTotalItem(totalItem - 1);
+    setProductsDataOrder(listData);
   }
 
   useEffect(() => {
-
     if (productsDataOrder.length > 0) {
-      let totalIp = fcTotalInputPrice(productsDataOrder);
-      setTotalInputPrice(totalIp);
-
-      let totalBuy = fcTotalBuyPrice(productsDataOrder);
-      setTotalBuyPrice(totalBuy);
-
-      let totalPt = totalBuy - totalIp;
-      setTotalProfit(totalPt);
+      let totalInp = fcTotalInputPrice(productsDataOrder);
+      setTotalInputPrice(totalInp);
+      let totalBuyPr = fcTotalBuyPrice(productsDataOrder);
+      setTotalBuyPrice(totalBuyPr);
+      let totalProfitPrice = totalBuyPr - totalInp;
+      setTotalProfit(totalProfitPrice);
     } else {
       setTotalInputPrice(0);
       setTotalBuyPrice(0);
       setTotalProfit(0);
     }
-    grid.refresh();
-    console.log('re-render');
-  }, [productsDataOrder])
+    //grid.refresh();
+  }, [totalItem])
 
   function removeObjectWithId(array, id) {
     if (array.length > 0) {
@@ -207,7 +198,7 @@ const Order = () => {
       });
 
       array.splice(indexOfObject, 1);
-      console.log(array);
+      setTotalItem(totalItem - 1);
       return array;
     }
   }
@@ -227,6 +218,7 @@ const Order = () => {
       object['product_code'] = objData['product_code'];
       object['product_name'] = objData['product_name'];
       object['quantity'] = objData['quantity'];
+      object['unit_name'] = objData['unit_name'];
       object['price'] = objData['input_price'];
       object['total_input_item'] = objData['total_input_item'];
       newArrayData.push(object);
@@ -238,7 +230,7 @@ const Order = () => {
     var fileName = 'Data' + Math.floor(Date.now() / 1000);
     const ws = XLSX.utils.json_to_sheet(newArrayData);
     /* custom headers */
-    XLSX.utils.sheet_add_aoa(ws, [["STT", "Mã sản phẩm", "Tên sản phẩm", "Số lượng", "Đơn giá", "Tổng tiền"]], { origin: "A1" });
+    XLSX.utils.sheet_add_aoa(ws, [["STT", "Mã sản phẩm", "Tên sản phẩm", "Số lượng", "Đơn vị", "Đơn giá", "Tổng tiền"]], { origin: "A1" });
     const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
     const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
     const data = new Blob([excelBuffer], { type: fileType });
@@ -256,17 +248,15 @@ const Order = () => {
       { field: 'idx', direction: 'descending' }
     ]
   };
-  const fields = { text: 'product_code', value: 'id' };
+
   return (
     <>
       <ToastContainer autoClose={3000} />
       <div className="md:m-8 mt-24 p-2 md:p-8 bg-white rounded-2xl">
         <Header category="" title="Order sản phẩm" />
-        <ComboBoxComponent showClearButton={true} floatLabelType="Auto" value={productOrder} width={'60%'}
-          onChange={e => setProductOrder(e.target.value)} fields={fields} dataSource={productsData}
-          allowCustom={true} placeholder="Chọn sản phẩm" />
+        <TextBoxComponent floatLabelType="Auto" value={productOrder} width={'30%'} onChange={e => setProductOrder(e.target.value)} placeholder="Mã sản phẩm" />
         <button className='ml-10 btn-add-product' style={{ backgroundColor: currentColor, marginLeft: '40px' }}
-          onClick={() => { checkOldPrice(productOrder) }}>Xem giá cũ</button>
+          onClick={() => { checkOldPrice() }}>Xem giá cũ</button>
         <br></br>
         <NumericTextBoxComponent floatLabelType="Auto" value={quantity} width={'30%'}
           validateDecimalOnType={true} decimals={0} format='n0'
@@ -301,7 +291,8 @@ const Order = () => {
             <ColumnDirective field='idx' width='0' headerText='' textAlign="Center" />
             <ColumnDirective field='product_code' width='60' headerText='Mã SP' textAlign="Left" />
             <ColumnDirective field='product_name' clipMode='EllipsisWithTooltip' width='100' headerText='Tên SP' textAlign="Left" />
-            <ColumnDirective field='quantity' width='60' headerText='Số lượng' textAlign="Center" />
+            <ColumnDirective field='quantity' width='40' headerText='SL' textAlign="Center" />
+            <ColumnDirective field='unit_name' width='30' headerText='ĐV' textAlign="Center" />
             <ColumnDirective field='input_price' width='50' headerText='Giá nhập' textAlign="Center" />
             <ColumnDirective field='buy_price' width='50' headerText='Giá bán' textAlign="Center" />
             <ColumnDirective field='total_input_item' width='50' headerText='Tiền nhập' textAlign="Center" />
@@ -326,11 +317,9 @@ const Order = () => {
         close={dialogClose} overlayClick={onOverlayClick} showCloseIcon={true} header='Thông tin giá sản phẩm' closeOnEscape={false}
         buttons={buttonDialogCheckPrice} position={positionDialog}>
         <div>
-          {isOldPrice ?
-            <span className='font-bold'>
-              {dataOldPrice}
-            </span>
-            : 'Không có thông tin giá cũ của sản phẩm !'}
+          <span className='font-bold'>
+            {dataOldPrice}
+          </span>
         </div>
       </DialogComponent>
     </>
